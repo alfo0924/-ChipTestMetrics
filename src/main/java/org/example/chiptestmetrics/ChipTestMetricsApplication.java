edb915a2cb51ad198542bbe3ad66a6b4774a0088
alfo0924 <halo0924@yahoo.com.tw>
2025-05-30 23:16:45 +0800

    v1.01 initial files upload

---
+++ src/main/java/org/example/chiptestmetrics/ChipTestMetricsApplication.java
@@ -0,0 +1,13 @@
+package org.example.chiptestmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChipTestMetricsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChipTestMetricsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/chiptestmetrics/ChipTestMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chiptestmetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChipTestMetricsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
